853c174b5c80e2d2b09821ad7559c21f0445393f
delentor86 <48762265+delentor86@users.noreply.github.com>
2020-10-16 13:29:47 -0500

    Update randomNumber.py

--- randomNumber.py
+++ randomNumber.py
@@ -1,16 +1,20 @@
-#random time generator that favors higher number
 def randomNumber():
-
-    import random
-    numberOne = random.randint(30,44)
-    numberTwo = random.randint(30,44)
     
-	#if statements to save higher of two die rolls
-    if numberOne >= numberTwo:
-        timerNumber = numberOne
-    else: 
-        timerNumber = numberTwo
+    import random
+    
+    rollOne = random.randint(30,44)
+    rollTwo = random.randint(30,44)
+    rollThree = random.randint(30,44)
+    
+    if rollOne > rollTwo and rollOne > rollThree: 
+        timerNumber = rollOne
+    elif rollTwo > rollThree and rollTwo > rollOne: 
+        timerNumber = rollTwo
+    else:
+        timerNumber = rollThree
     print(timerNumber) 
-	
-#function call for randomNumber
+    
+
 randomNumber()
+
+
