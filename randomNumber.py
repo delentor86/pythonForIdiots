7d3dd8d34cb8d5635e4100d0558f4f12355075a0
delentor86 <48762265+delentor86@users.noreply.github.com>
2020-10-17 14:44:49 -0500

    Update randomNumber.py

--- randomNumber.py
+++ randomNumber.py
@@ -1,22 +1,32 @@
-#getting variable for random minute value for timer
-def randomNumber():
-    
-    import random
-    
-    rollOne = random.randint(30,44)
-    rollTwo = random.randint(30,44)
-    rollThree = random.randint(30,44)
-    
-    if rollOne > rollTwo and rollOne > rollThree: 
-        timerNumber = rollOne
-    elif rollTwo > rollThree and rollTwo > rollOne: 
-        timerNumber = rollTwo
-    else:
-        timerNumber = rollThree
-    return(timerNumber)
+#This original terrible code written by delentor86@gmail.com 
 
-#main function will call randomNumber function and display results
-def main():
-    timer = randomNumber()
-    print(timer)
-main()
+#creating timer object
+class Alarm:
+	def __init__(self, timers):
+
+
+
+#random time generator that favors higher number
+def randomNumber():
+
+    import random
+	#first variable
+    numberOne = random.randint(30,44)
+	#second variable
+    numberTwo = random.randint(30,44)
+    #third roll
+    numberThree = random.randint(30,44)
+    
+	#compare the variables and keep the highest
+    if numberOne > numberTwo and numberOne > numberThree:
+        timerNumber = numberOne
+        
+    elif numberTwo > numberOne and numberTwo > numberThree:
+        timerNumber = numberTwo
+        
+    else: 
+        timerNumber = numberThree
+        
+    return(timerNumber)
+	
+    #function call for randomNumber
